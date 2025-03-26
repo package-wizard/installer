67ecacefd9932698c1ade87b2150bbcd9e75d019
Andrey Helldar <helldar@dragon-code.pro>
2025-03-26 12:41:29 +0300

    Fix for placeholder

--- app/Data/Questions/QuestionAskTextData.php
+++ app/Data/Questions/QuestionAskTextData.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PackageWizard\Installer\Data\Questions;
 
 use PackageWizard\Installer\Data\Casts\ArrayWrapCast;
+use PackageWizard\Installer\Data\Casts\PlaceholderCast;
 use PackageWizard\Installer\Data\Casts\ToStringCast;
 use PackageWizard\Installer\Data\Casts\TranslatableCast;
 use PackageWizard\Installer\Data\Casts\ValidationCast;
@@ -27,7 +28,7 @@ class QuestionAskTextData extends QuestionData
     #[WithCast(ToStringCast::class)]
     public string $default = '';
 
-    #[WithCast(TranslatableCast::class)]
+    #[WithCast(PlaceholderCast::class)]
     public string $placeholder = '';
 
     public bool|string $required = true;
